5d22775e6e01b269aa718251d581049ff9ef0350
glitchninja2000 <glitchninja2000@gmail.com>
2016-03-07 14:20:47 -0800

    Create theme.js
    Uses search query to change the theme of the website. Includes all planned themes, both used and unused.

---
+++ query/theme.js
@@ -0,0 +1,61 @@
+// Themes the UNDERTML site pages.
+// theme = str
+var = q(str);
+
+// Basic Themes
+  else if (q = 'Default') {
+  }
+  else if (q = 'Debug') {
+  }
+  else if (q = 'Dark') {
+  }
+  else if (q = 'Light') {
+  }
+  else if (q = 'TobyFox') {
+  }
+  else if (q = 'Dogs') {
+  }
+
+// Location Themes
+  else if (q = 'Ruins') {
+  }
+  else if (q = 'TobyFox') {
+  }
+
+// Character Themes
+  else if (q = 'Frisk') {
+  }
+  else if (q = 'Chara') {
+  }
+  else if (q = 'Flowey') {
+  }
+  else if (q = 'Toriel') {
+  }
+  else if (q = 'Sans') {
+  }
+  else if (q = 'Papyrus') {
+  }
+  else if (q = 'Undyne') {
+  }
+  else if (q = 'Alphys') {
+  }
+  else if (q = 'Mettaton') {
+  }
+  else if (q = 'Asgore') {
+  }
+  else if (q = 'TobyFox') {
+  }
+
+// Bonus Themes
+  else if (q = 'JackSepticEye') {
+  }
+  else if (q = 'PewDiePie') {
+  }
+  else if (q = 'The Game Theorist' || q = 'MatthewPatrick13') {
+  }
+  else if (q = 'TobyFox') {
+  }
+
+// Else Code (for errors)
+  else {
+  }
