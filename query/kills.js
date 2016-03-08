572dc7f6c67632dc4b1e0d6ff2a8fcb45f44bc97
glitchninja2000 <glitchninja2000@gmail.com>
2016-03-07 21:48:26 -0800

    Create kills.js

---
+++ query/kills.js
@@ -0,0 +1,7 @@
+var kills = q('kills'); // Monsters Killed
+var ik = q('ik'); // Flowey (nk for spare; everyone else change the 'k' to 's')
+var tk = q('tk'); // Toriel
+var sk = q('sk'); // Sans
+var pk = q('pk'); // Papyrus (pd for dated)
+// Undyne (ud for befriended)
+// Alphys (ad for dated)
