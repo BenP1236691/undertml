5af581efd2318502ccd7fad6909fc42b67c370cd
glitchninja2000 <glitchninja2000@gmail.com>
2016-03-07 19:03:49 -0800

    Create game-beta.js

---
+++ query/game-beta.js
@@ -0,0 +1,10 @@
+var mode = q('mode'); // 'default','flowey','beta' 
+var dialog = q('dialog'); // 'enabled','disabled','true','false','on','off','yes','no','1','0'
+var textcolor = q('textcolor'); // (css code)
+var textfont = q('textfont'); // (font name),'sans-seriff','comic-sans','papyrus','wingdings'
+var username = q('username'); // (username)
+var user = q('user'); // (username)
+var keycode = q('keycode'); // (keycode)
+var key = q('key'); // (keycode)
+var code = q('code'); (unlock code)
+var refer = q('refer'); // (referal code)
