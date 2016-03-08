e04f4aa5baa1e52a776f1588bda7c77f87b26811
glitchninja2000 <glitchninja2000@gmail.com>
2016-03-08 09:09:34 -0800

    Update game-flags.js

--- query/game-flags.js
+++ query/game-flags.js
@@ -1,5 +1,38 @@
-var flag
-var flagId
-var flagName
-var flagVal
-var flags
+try {
+// START OF DOCUMENT CODE
+// Not all flags are or will be supported.
+
+// Blank Flag (no function)
+var flag[] = q('flag[]')
+var flag = q('flag')
+var flg = q('flg')
+
+// Flag 'var' Values
+var flag[5] = q('flag[5]') // (1 - 100)
+var flag5 = q('flag5')
+var fun = q('fun')
+var flag[6] = q('flag[6]')
+var flag6 = q('flag6')
+var hardmode = q('hardmode')
+
+// Blank Flag Script (no function)
+if (flg = flg || flag = flag || flag[] = flag[]) {
+  
+}
+
+// Flag[5] or 'fun' Scripts
+if (fun = '66' || flag5 = '66' || flag[5] = '66') {
+  
+}
+
+// Flag Scripts
+// Not all flag variables are or will be used.
+
+// END OF DOCUMENT CODE
+}
+// debug
+catch(err) {
+  var p = document.createElement("p");
+  p.innerHTML = 'query/game-flags.js'+err.message;
+  document.getElementById("bugboxprint").appendChild(p);
+}
