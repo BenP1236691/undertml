13a2c05db060651afcf27333c93ceb53893413e2
glitchninja2000 <glitchninja2000@gmail.com>
2016-03-10 13:07:15 -0800

    Update site.js

--- query/site.js
+++ query/site.js
@@ -1,8 +1,14 @@
 // Edits the main UNDERTML site pages
 // vars
 var title = q('title')
+var soul = q('soul')
 
 // scripts
 if (title != 'err404') {
-  document.getElementsByTagName("title")[0].innerHTML = title; 
+  setTitle(title) 
+}
+if (soul != 'err404') {
+  var directory = 'images';
+  var soulURL = directory+'/soul'+soul+'.png';
+  setIcon(soulURL);
 }
